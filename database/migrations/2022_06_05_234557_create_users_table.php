4053c1c8e134eda7c8789d5a74381db8ce994cdb
真弘 小田倉 <masahiroodakura@masahironoMacBook-Air.local>
2022-06-13 22:45:45 +0900

    controller,migrationファイル,model大枠実装

---
+++ database/migrations/2022_06_05_234557_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('name');
+            $table->unsignedBigInteger('age')->nullable();
+            $table->boolean('sex')->nullable();
+            $table->string('facebook_id')->nullable();
+            $table->string('password')->nullable();
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2022_06_06_125503_create_item_room_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_room', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id');
+            $table->foreignId('room_id');
+            $table->dateTime('using_time');
+            $table->unsignedBigInteger('status_id');
+            $table->timestamps();
+
+            $table
+            ->foreign('item_id')
+            ->references('id')
+            ->on('items')
+            ->onDelete('cascade');
+
+            $table
+            ->foreign('room_id')
+            ->references('id')
+            ->on('rooms')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_room');
+    }
+};
