db2fcef40486d07643e183091632b1f5d8def21e
真弘 小田倉 <masahiroodakura@masahironoMacBook-Air.local>
2022-06-16 23:47:24 +0900

    route修正

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
