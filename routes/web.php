4d94ef20406f629056dd80479b31f1f5b027420b
真弘 小田倉 <masahiroodakura@masahironoMacBook-Air.local>
2022-06-17 12:56:20 +0900

    viewエラー格闘中

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Auth\LoginController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('masahiro');
 });
 
 Route::get('login/facebook', [LoginController::class, 'redirectToProvider']);
