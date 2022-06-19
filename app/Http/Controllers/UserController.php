00fe400e83f4fdd135f87ace3ef57de3aec5ff09
真弘 小田倉 <masahiroodakura@masahironoMacBook-Air.local>
2022-06-19 09:56:40 +0900

    firebase格闘

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Log;
-// use Kreait\Firebase;
+use Kreait\Firebase;
 
 class UserController extends Controller
 {
