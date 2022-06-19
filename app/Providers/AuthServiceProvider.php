80d4e44c230658d94a0b6641af31b42f1bf5f109
真弘 小田倉 <masahiroodakura@masahironoMacBook-Air.local>
2022-06-19 19:37:22 +0900

    auth格闘/gitignore変更

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,6 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         if (! $this->app->routesAreCached()) {
             Passport::routes();
         }
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
         //
     }
 }
