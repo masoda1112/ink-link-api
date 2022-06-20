239c78498400ecdf2f0abb4d6b0054f35280cfd4
真弘 小田倉 <masahiroodakura@masahironoMacBook-Air.local>
2022-06-20 17:37:56 +0900

    usershowでid取得するコード修正

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Room;
 use \GuzzleHttp\Client;
 // use Illuminate\Support\Facades\Auth;
 use Kreait\Firebase\Contract\Auth;
