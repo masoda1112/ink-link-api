f9d8790f527b216d11887365e0f79736fac76203
真弘 小田倉 <masahiroodakura@masahironoMacBook-Air.local>
2022-06-20 23:04:39 +0900

    room作成の格闘

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Item;
 use App\Models\Room;
 use \GuzzleHttp\Client;
 // use Illuminate\Support\Facades\Auth;
