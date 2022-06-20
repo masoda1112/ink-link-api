ce7e045a0227baaa86350f62f5ad7aa67bfbaa11
真弘 小田倉 <masahiroodakura@masahironoMacBook-Air.local>
2022-06-20 21:10:59 +0900

    room作成の格闘

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -37,6 +37,7 @@ class RoomController extends Controller
             if(!$room){
                 $this->createHelper($request);
             }else{
+                dd($room);
                 $this->addUser($request, $room);
             }
         } catch(Exeption $e){
